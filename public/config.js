ba61d73a5df078279b77869adc4c9531c663c0f5
Cutiei <140823642+Cutiei@users.noreply.github.com>
2024-06-19 21:02:35 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
@@ -16,7 +16,7 @@ window.Config = {
   CountDays: 90,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
